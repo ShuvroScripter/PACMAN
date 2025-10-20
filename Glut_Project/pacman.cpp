b984e9735c160466b1148aec62c563343240ceb2
Tridip Barua Shuvro <157264364+ShuvroScripter@users.noreply.github.com>
2025-10-20 20:29:13 +0600

    updated function draw pacman,pallets,text

--- Glut_Project/pacman.cpp
+++ Glut_Project/pacman.cpp
@@ -61,3 +61,66 @@ void initializeGhosts() {
     ghosts.push_back(ghost3);
     ghosts.push_back(ghost4);
 }
+void resetGame() {
+    pacmanX = 1;
+    pacmanY = 1;
+    dirX = 0;
+    dirY = 0;
+    score = 0;
+    lives = 3;
+    gameOver = false;
+    gameWon = false;
+
+    // Reset maze pellets
+    for (int y = 0; y < ROWS; y++) {
+        for (int x = 0; x < COLS; x++) {
+            if (maze[y][x] == 2) {
+                maze[y][x] = 0;
+            }
+        }
+    }
+
+    initializeGhosts();
+}
+
+void drawSquare(int x, int y, float r, float g, float b) {
+    glColor3f(r, g, b);
+    glBegin(GL_QUADS);
+    glVertex2f(x * CELL, y * CELL);
+    glVertex2f((x + 1) * CELL, y * CELL);
+    glVertex2f((x + 1) * CELL, (y + 1) * CELL);
+    glVertex2f(x * CELL, (y + 1) * CELL);
+    glEnd();
+}
+
+void drawText(float x, float y, string text) {
+    glColor3f(1, 1, 1);
+    glRasterPos2f(x, y);
+    for (char c : text)
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+}
+
+void drawPellets() {
+    glColor3f(1.0, 1.0, 0.5);
+    for (int y = 0; y < ROWS; y++) {
+        for (int x = 0; x < COLS; x++) {
+            if (maze[y][x] == 0) {
+                glPointSize(5);
+                glBegin(GL_POINTS);
+                glVertex2f(x * CELL + CELL / 2, y * CELL + CELL / 2);
+                glEnd();
+            }
+        }
+    }
+}
+
+void drawPacman() {
+    glColor3f(1.0, 1.0, 0.0);
+    glBegin(GL_POLYGON);
+    for (int i = 0; i < 360; i++) {
+        float theta = i * 3.14159f / 180;
+        glVertex2f(pacmanX * CELL + CELL/2 + 15 * cos(theta),
+                   pacmanY * CELL + CELL/2 + 15 * sin(theta));
+    }
+    glEnd();
+}
